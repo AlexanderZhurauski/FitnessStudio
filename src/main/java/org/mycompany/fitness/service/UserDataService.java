0c123d4f921ced8eb8de05474ccb8e6c7d43de8c
AlexanderZhurauski <gandalfdude@gmail.com>
2023-02-22 17:19:36 +0300

    created user DDL and DML and deleted redundant converter

--- src/main/java/org/mycompany/fitness/service/UserDataService.java
+++ src/main/java/org/mycompany/fitness/service/UserDataService.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.mycompany.fitness.service.api.IUserDataService;
 
-import java.time.Instant;
 import java.util.UUID;
 
 public class UserDataService implements IUserDataService {
